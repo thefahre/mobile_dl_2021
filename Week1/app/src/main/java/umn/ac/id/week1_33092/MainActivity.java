ba9f972dfe0ee2f9d13fe31e70bee690e5c08e2d
Fahreza Ali Syahdani <fahreza.ali@student.umn.ac.id>
2021-01-26 12:24:40 +0700

    Week 1

---
+++ Week1/app/src/main/java/umn/ac/id/week1_33092/MainActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.week1_33092;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
